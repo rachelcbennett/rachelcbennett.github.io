986b9dde9406eb449417efe98e77d1d9b38be3fb
Rachel Bennett <rachel.cardon9@gmail.com>
2021-02-22 23:50:26 -0700

    Added db

--- docs/index.js
+++ docs/index.js
@@ -12,5 +12,23 @@ function storeData(){
     .catch((error) => {
         console.error("Error adding document: ", error);
     });
-    
+
+}
+
+function storeComment(){
+    console.log("entered storecomment")
+    var commenter_name = document.getElementById("nameInput").value;
+    var commenter_email = document.getElementById("emailInput").value;
+    var commenter_comment= document.getElementById("commentInput").value;
+    console.log(commenter_name)
+    db.collection("comments").doc("new-id").set({
+        name: "herro",
+        comment: "herro there"
+    })
+    .then((docRef) => {
+        console.log("Document written with ID: ", docRef.id);
+    })
+    .catch((error) => {
+        console.error("Error adding document: ", error);
+    });
 }
