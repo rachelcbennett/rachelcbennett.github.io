5d764fda20a6073b1c8fd5e8cf8eb4ba19a0754e
Rachel Bennett <rachel.cardon9@gmail.com>
2021-02-26 21:31:39 -0700

    Added way to get comments

--- docs/auth.js
+++ docs/auth.js
@@ -2,6 +2,8 @@
 const txtEmail = document.getElementById('txtEmail');
 const txtPassword = document.getElementById('txtPassword');
 const btnLogin = document.getElementById('btnLogin');
+var db = firebase.firestore();
+var html_comments = document.getElementById('html_comments')
 
 
 btnLogin.addEventListener('click', e=> {
@@ -13,3 +15,27 @@ btnLogin.addEventListener('click', e=> {
     const promise = auth.signInWithEmailAndPassword(email, passwd);
     promise.catch(e=> console.log("Error: ", e.message))
 })
+
+function GetComments(){
+    db.collection("comments").get().then((querySnapshot) => {
+        results=""
+        querySnapshot.forEach((doc) => {
+            // doc.data() is never undefined for query doc snapshots
+            console.log(doc.id, " => ", doc.data());
+            results += "<div class='container comment'>";
+            results+="<h3>"
+            results+=doc.data().name;
+            results+= " says:";
+            results+="</h3>"
+            results+="<br>";
+            results+=doc.data().comment;
+            results+="<br>"
+            results+= doc.data().date;
+            
+            results+="</div>"
+            results+="<div class='space'> </div>"
+            
+        });
+        document.getElementById("html_comments").innerHTML = results; //change for textcontent
+    });
+}
--- docs/index.js
+++ docs/index.js
@@ -21,11 +21,22 @@ function storeComment(){
     comment_input = document.getElementById("commentInput").value;
     name_input = document.getElementById("nameInput").value;
     email_input = document.getElementById("emailInput").value;
+    let currentDate = new Date();
+    let Day = currentDate.getDate()
+    let Month = currentDate.getMonth() + 1
+    let Year = currentDate.getFullYear()
+    let curr_date = Month + "/" + Day +"/" + Year
+
+
+
     db.collection("comments").doc(email_input).set({
         name: name_input,
-        comment: comment_input
+        comment: comment_input,
+        page: document.title,
+        date: curr_date
     })
     .then(() => {
+        alert("Success!");
         console.log("Document written with ID: ", email_input);
     })
     .catch((error) => {
